ef6da3302cd9f57db2f9ccd52904dda55a89da1a
jjwoolley <jonnywoolley2@gmail.com>
2020-08-29 18:57:05 -0700

    Updated summary stats and described plan

--- titanic.R
+++ titanic.R
@@ -13,6 +13,8 @@
 # embarked (port of Embarkation)
     # C = Cherbourg, Q = Queenstown, S = Southampton
 
+# Loading Data------------------------------------------------------------------
+
 
 library("tidyverse")
 library("haven")
@@ -29,26 +31,69 @@ titanic <- read_csv ("train.csv") %>%
 
 original <- titanic
 
+# PLAN--------------------------------------------------------------------------
+
+# cross folds (v = 5)
+# run a few different models 
+# (logistic, lasso logistic, ridge logistic, K-NN,
+# Linear Discriminant Analysis, LDA, QDA, PCA)
+# Determine which has the lowest classification error and submit the top few
+
+# Issues
+# Missing data: How to deal with missing data for age
+# Variable Selection: Which to use and which not to use
+
+
+
+
+
+
+
+# Descriptive Stats-------------------------------------------------------------
 
 # get quick look at data format
 head(titanic)
 str(titanic)
+
 # check for NA's in each column
 map(titanic, ~sum(is.na(.)))
 # Age (177), Cabin (687), Embarked (2)
 
 # same last names might have better chance of surviving
 
-# graph survivability based on gender and ticket class
+# survival rate based on gender and ticket class
 titanic %>%
   group_by(Sex, Pclass) %>%
   summarise(mean = mean(Survived))
 
+# men vs women
 titanic %>%
-  summarise(Age, Sex)
+  count(Sex)
+
+# look at distribution of age
+titanic %>%
+  summarise(mean_age = mean(Age, na.rm = T),
+            min = quantile(Age, probs = 0, na.rm = T),
+            "0.25" = quantile(Age, probs = 0.25, na.rm = T),
+            median = quantile(Age, probs = 0.5, na.rm = T),
+            "0.75" = quantile(Age, probs = 0.75, na.rm = T),
+            max = max(Age, na.rm = T))
 
 titanic %>%
   summarise_if(is.numeric, mean, na.rm =T)
 
 titanic %>%
   summarise_if(is.numeric, quantile, na.rm =T)
+
+
+
+
+# Models------------------------------------------------------------------------
+fit1.lm <- lm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+              data = titanic)
+summary(fit1.lm)
+
+fit2.log <- glm(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked,
+                family = "binomial",
+                data = titanic)
+summary(fit2.log)
